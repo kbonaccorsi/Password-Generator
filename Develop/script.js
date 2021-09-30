edab93060723207963512fafc14fb53c2717bcf0
Kaileigh Bonaccorsi <kaileigh.bonaccorsi@gmail.com>
2021-09-30 09:31:43 -0500

    password length prompt, confirmation boxes

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,33 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+function length() {
 var length = prompt("Choose a password length between 8 and 128 characters.");
-if (length <8) {
+  if (length >= 8 && length <= 128) {
+  confirm("Your password will contain lowercase characters.");
+} else {
   prompt("Choose a password length between 8 and 128 characters.");
-}else if (length >128) {
-  prompt("Choose a password length between 8 and 128 characters.");
-}else {
-  confirm("My password should contain lowercase letters.")
-};
+}
+var confirmLower = confirm("Your password will contain uppercase characters.");
+if (confirmLower === true) {
+  confirm("Your password will contain numeric characters.");
+} else {
+  confirm("You must choose ok to continue generating a password.");
+}
+var confirmUpper = confirm("Your password will contain numeric characters.");
+if (confirmUpper === true) {
+  confirm("Your password will contain special characters.");
+} else {
+  confirm("You must choose ok to continue generating a password.");
+}
+}
 
 
-//User inputs length of password 8-128: prompt method
+length ("");
+// user input between 8-128, repeat until user sets correct amount
+
+
 //User says yes to different character types: confirm method
 //loop over length call generator function for each type
 //add final password to the password var and return in alert
