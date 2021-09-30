385b07865538aa2e443f2c17383b84ca5a09d217
Kaileigh Bonaccorsi <kaileigh.bonaccorsi@gmail.com>
2021-09-29 23:42:19 -0500

    set initial prompt for password length, and confirmation with acceptable length

--- Develop/script.js
+++ Develop/script.js
@@ -1,27 +1,78 @@
-//https://www.w3schools.com/html/html_charset.asp
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+var length = prompt("Choose a password length between 8 and 128 characters.");
+if (length <8) {
+  prompt("Choose a password length between 8 and 128 characters.");
+}else if (length >128) {
+  prompt("Choose a password length between 8 and 128 characters.");
+}else {
+  confirm("My password should contain lowercase letters.")
+};
+
+
+//User inputs length of password 8-128: prompt method
+//User says yes to different character types: confirm method
+//loop over length call generator function for each type
+//add final password to the password var and return in alert
+
+//Object containing 4 password criteria
+const characterType = {
+  length: randomizeNumeric,
+  lower: randomizeLower,
+  upper: randomizeUpper,
+  numeric: randomizeNumeric,
+  specialCharacter: randomizeSpecialCharacter,
+  };
+
+
+  function generatePassword (length, lower, upper, numeric, specialCharacter) {
+    var length = [];
+for (var i=8; i <=128; i++){
+  length.push(i);
+    };
+
+
+
+var generatePassword = "";
+
+
+
+
 
 // Generator functions
 
-function lower() {
+function randomizeLower() {
   const lowercase = "qwertyuiopasdfghjklzxcvbnm";
   return lowercase [Math.floor(Math.random() * lowercase.length)];
 }
 
-function upper() {
+function randomizeUpper() {
   const uppercase = "QWERTYUIOPASDFGHJKLZXCVBNM";
   return uppercase [Math.floor(Math.random() * uppercase.length)];
 }
 
-function numeric() {
+function randomizeNumeric() {
   const digits = "1234567890";
   return digits [Math.floor(Math.random() * digits.length)];
 }
 
-function specialCharacter() {
+function randomizeSpecialCharacter() {
   const characters = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
   return characters [Math.floor(Math.random() * characters.length)];
 }
-console.log(lower());
-console.log(upper());
-console.log(numeric());
-console.log(specialCharacter())
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+alert(writePassword);
+}
