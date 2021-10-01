ab0e859a7c77338f14e0156d17671e648021fb03
Kaileigh Bonaccorsi <kaileigh.bonaccorsi@gmail.com>
2021-09-30 19:08:50 -0500

    confirm dialogue boxes function

--- Develop/script.js
+++ Develop/script.js
@@ -2,62 +2,6 @@
 var generateBtn = document.querySelector("#generate");
 
 
-function length() {
-var length = prompt("Choose a password length between 8 and 128 characters.");
-  if (length >= 8 && length <= 128) {
-  confirm("Your password will contain lowercase characters.");
-} else {
-  prompt("Choose a password length between 8 and 128 characters.");
-}
-var confirmLower = confirm("Your password will contain uppercase characters.");
-if (confirmLower === true) {
-  confirm("Your password will contain numeric characters.");
-} else {
-  confirm("You must choose ok to continue generating a password.");
-}
-var confirmUpper = confirm("Your password will contain numeric characters.");
-if (confirmUpper === true) {
-  confirm("Your password will contain special characters.");
-} else {
-  confirm("You must choose ok to continue generating a password.");
-}
-}
-
-
-length ("");
-// user input between 8-128, repeat until user sets correct amount
-
-
-//User says yes to different character types: confirm method
-//loop over length call generator function for each type
-//add final password to the password var and return in alert
-
-//Object containing 4 password criteria
-const characterType = {
-  length: randomizeNumeric,
-  lower: randomizeLower,
-  upper: randomizeUpper,
-  numeric: randomizeNumeric,
-  specialCharacter: randomizeSpecialCharacter,
-  };
-
-
-  function generatePassword (length, lower, upper, numeric, specialCharacter) {
-    var length = [];
-for (var i=8; i <=128; i++){
-  length.push(i);
-    };
-
-
-
-var generatePassword = "";
-
-
-
-
-
-// Generator functions
-
 function randomizeLower() {
   const lowercase = "qwertyuiopasdfghjklzxcvbnm";
   return lowercase [Math.floor(Math.random() * lowercase.length)];
@@ -78,6 +22,75 @@ function randomizeSpecialCharacter() {
   return characters [Math.floor(Math.random() * characters.length)];
 }
 
+const characterType = {
+  length: length,
+  lower: randomizeLower,
+  upper: randomizeUpper,
+  numeric: randomizeNumeric,
+  specialCharacter: randomizeSpecialCharacter,
+  };
+
+
+//make sure order follows: global variables, functions, process
+
+// user input between 8-128, repeat until user sets correct amount
+
+
+
+//loop over length call generator function for each type
+//add final password to the password var and return in alert
+
+//Object containing 4 password criteria
+
+  function generatePassword() {
+
+    var length = prompt("Choose a password length between 8 and 128 characters.");
+    if (length >= 8 && length <= 128) {
+      confirm("Your password will contain lowercase characters.");
+    } else {
+      prompt("Choose a password length between 8 and 128 characters.");
+    }
+  
+  var lowercasePrompt = confirm("Your password will contain lowercase characters.");
+    if (lowercasePrompt === true) {
+      confirm("Your password will contain uppercase characters.");
+    } else {
+      confirm("You must choose ok to continue generating a password.");
+    }
+  
+  var uppercaseConfirm = confirm("Your password will contain uppercase characters.");
+    if (uppercaseConfirm === true) {
+      confirm("Your password will contain numeric characters.");
+      } else {
+      confirm("You must choose ok to continue generating a password.");
+    }
+  
+  var numericConfirm = confirm("Your password will contain numeric characters.");
+      if (numericConfirm === true) {
+      confirm("Your password will contain special characters.");
+    } else {
+      confirm("You must choose ok to continue generating a password.");
+    }
+  
+  var specialCharacterConfirm = confirm("Your password will contain special characters.");
+    if (specialCharacterConfirm === true) {
+      alert("You're randomly generated password is" + writePassword);
+    } else {
+      confirm("You must choose ok to continue generating a password.");
+    };
+
+
+
+var generatePassword = "";
+  }
+
+
+
+
+// Generator functions
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -89,6 +102,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-alert(writePassword);
-}
