ba309e904af4ca7fff6af3f8bb5e1d370e032b14
Kaileigh Bonaccorsi <kaileigh.bonaccorsi@gmail.com>
2021-09-30 21:57:39 -0500

    ready to work on loop for length of password, and actual generation

--- Develop/script.js
+++ Develop/script.js
@@ -1,94 +1,73 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//all characters that will go into the password
+const lowercase = "qwertyuiopasdfghjklzxcvbnm".split("");
+const uppercase = "QWERTYUIOPASDFGHJKLZXCVBNM".split ("");
+const numeric = "1234567890".split("");
+const symbol = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~".split("");
+var passwordArr = [];
 
-function randomizeLower() {
-  const lowercase = "qwertyuiopasdfghjklzxcvbnm";
-  return lowercase [Math.floor(Math.random() * lowercase.length)];
-}
+//function that will generate a password based on multiple inputs from the user
+function generatePassword() {
 
-function randomizeUpper() {
-  const uppercase = "QWERTYUIOPASDFGHJKLZXCVBNM";
-  return uppercase [Math.floor(Math.random() * uppercase.length)];
-}
+  var length = prompt("Choose a password length between 8 and 128 characters.");
 
-function randomizeNumeric() {
-  const digits = "1234567890";
-  return digits [Math.floor(Math.random() * digits.length)];
-}
-
-function randomizeSpecialCharacter() {
-  const characters = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
-  return characters [Math.floor(Math.random() * characters.length)];
-}
-
-const characterType = {
-  length: length,
-  lower: randomizeLower,
-  upper: randomizeUpper,
-  numeric: randomizeNumeric,
-  specialCharacter: randomizeSpecialCharacter,
-  };
-
-
-//make sure order follows: global variables, functions, process
-
-// user input between 8-128, repeat until user sets correct amount
-
-
-
-//loop over length call generator function for each type
-//add final password to the password var and return in alert
-
-//Object containing 4 password criteria
-
-  function generatePassword() {
-
-    var length = prompt("Choose a password length between 8 and 128 characters.");
-    if (length >= 8 && length <= 128) {
-      confirm("Your password will contain lowercase characters.");
-    } else {
-      prompt("Choose a password length between 8 and 128 characters.");
-    }
-  
-  var lowercasePrompt = confirm("Your password will contain lowercase characters.");
+  if (length <= 8 || length >= 128) {
+    alert("Choose a password length between 8 and 128 characters.");
+    generatePassword();
+  } else {
+    
+    var lowercasePrompt = confirm("Do you want your password to contain lowercase characters?");
     if (lowercasePrompt === true) {
-      confirm("Your password will contain uppercase characters.");
+      alert("Your password will contain lowercase characters.");
+      // add lowercase characters to the array
+    passwordArr.push(...lowercase);
     } else {
-      confirm("You must choose ok to continue generating a password.");
-    }
-  
-  var uppercaseConfirm = confirm("Your password will contain uppercase characters.");
-    if (uppercaseConfirm === true) {
-      confirm("Your password will contain numeric characters.");
-      } else {
-      confirm("You must choose ok to continue generating a password.");
-    }
-  
-  var numericConfirm = confirm("Your password will contain numeric characters.");
-      if (numericConfirm === true) {
-      confirm("Your password will contain special characters.");
-    } else {
-      confirm("You must choose ok to continue generating a password.");
-    }
-  
-  var specialCharacterConfirm = confirm("Your password will contain special characters.");
-    if (specialCharacterConfirm === true) {
-      alert("You're randomly generated password is" + writePassword);
-    } else {
-      confirm("You must choose ok to continue generating a password.");
+      alert("Your password will NOT contain lowercase characters.");
     };
 
+    var uppercasePrompt = confirm("Do you want your password to contain uppercase characters?");
+    if (uppercasePrompt === true) {
+      alert("Your password will contain uppercase characters.");
+      // add uppercase characters to the array
+    passwordArr.push(...uppercase);
+    } else {
+      alert("Your password will NOT contain uppercase characters.");
+    };
+
+    var numericPrompt = confirm("Do you want your password to contain numeric characters?");
+    if (numericPrompt === true) {
+      alert("Your password will contain numeric characters.");
+      // add numeric characters to the array
+    passwordArr.push(...numeric);
+    } else {
+      alert("Your password will NOT contain numeric characters.");
+    };
+  
+    var symbolPrompt = confirm("Do you want your password to contain symbol characters?");
+    if (symbolPrompt === true) {
+      alert("Your password will contain symbol characters.");
+      // add numeric characters to the array
+    passwordArr.push(...symbol);
+    } else {
+      alert("Your password will NOT contain symbol characters.");
+    };
+  };
+  
+  
+  passwordArr= "";
 
 
-var generatePassword = "";
-  }
+return passwordArr[Math.floor(Math.random() * passwordArr.length)];
 
 
 
+  //loop through the passwordArr
+  //add each character which got randomized to the generatePassword arr
+  
 
-// Generator functions
-
+};
 
 
 // Write password to the #password input
